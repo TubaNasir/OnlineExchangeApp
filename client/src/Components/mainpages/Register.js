e7fb15696d4e3310592018ffceb7bf9497e64d67
Sukaina Batool <sukainasbb@gmail.com>
2022-05-29 17:38:40 +0500

    commit by sukaina

--- client/src/Components/mainpages/Register.js
+++ client/src/Components/mainpages/Register.js
@@ -18,7 +18,7 @@ function Register() {
 
     const registerSubmit = async e => {
         e.preventDefault()
-        registerAPI({...user})
+        registerAPI({ ...user })
             .then(res => {
                 console.log(res.data)
                 alert('Registered Successfully')
@@ -33,110 +33,136 @@ function Register() {
 
     return (
         <div className='container col-md-6'  >
-            <div className='form-div'>
+            <div className='card mt-4'>
 
-                <form onSubmit={registerSubmit}>
-                    <text>
-                        Full Name
+                <div className="card-header">
+                    <h4>Register</h4>
+                </div>
+                <div className='card-body'>
+                    <form onSubmit={registerSubmit}>
+                        <div className='form-group mb-3'>
+                            <text>
+                                Full Name
+                            </text>
+                            <input type='text'
+                                name='name'
+                                value={user.name}
+                                required
+                                onChange={onChangeInput}
+                                className='form-control' />
+                        </div>
+
+                        <div className='form-group mb-3'>
+                            <text>
+                                Email
+                            </text>
+                            <input type='text'
+                                name='email'
+                                value={user.email}
+                                required
+                                onChange={onChangeInput}
+                                className='form-control' />
+                        </div>
+
+                        <div className='form-group mb-3'>
+                            <text>
+                                Password
+                            </text>
+                            <input type='password'
+                                //placeholder='Password'
+                                name='password'
+                                value={user.password}
+                                required
+                                onChange={onChangeInput}
+                                className='form-control form-group' />
+                        </div>
+
+                        <div className='form-group mb-3'>
+                            <text>
+                                Gender
+                            </text>
+
+                            <div className="form-check form-check-inline">
+                                <input type="radio"
+                                    id="gender1"
+                                    name='gender'
+                                    value='male'
+                                    onChange={onChangeInput}
+                                    className='form-check-input' />
+                                <label className="form-check-label" htmlFor="gender1">Male</label>
+                            </div>
+
+                            <div className="form-check form-check-inline">
+                                <input type="radio"
+                                    id="gender2"
+                                    name='gender'
+                                    value='female'
+                                    onChange={onChangeInput}
+                                    className='form-check-input' />
+                                <label className="form-check-label" htmlFor="gender2">Female</label>
+                            </div>
+                        </div>
+
+                        <div className='form-group mb-3'>
+                            <text>
+                                Contact
+                            </text>
+                            <input type='number'
+                                //placeholder='Contact'
+                                name='contact'
+                                value={user.contact}
+                                required
+                                onChange={onChangeInput}
+                                className='form-control form-group' />
+                        </div>
+
+                        <div className='form-group mb-3'>
+                            <text>
+                                City
+                            </text>
+                            <input type='text'
+                                //placeholder='City'
+                                name='city'
+                                value={user.city}
+                                required
+                                onChange={onChangeInput}
+                                className='form-control form-group' />
+                        </div>
+
+                        <div className='form-group mb-3'>
+                            <text>
+                                Province
+                            </text>
+                            <input type='text'
+                                //placeholder='Province'
+                                name='province'
+                                value={user.province}
+                                required
+                                onChange={onChangeInput}
+                                className='form-control form-group' />
+                        </div>
+
+                        <div className='row'>
+                            <div className='col-md-8 form-group mb-3'>
+                                <label>Image</label>
+                                <input type="file" name='image' value={user.image} onChange={onChangeInput} className='form-control form-group' />
+                            </div>
+                        </div>
+
+                        <div className='form-group mb-3'>
+                            <button type='submit'
+                                className='btn btn-danger px-4 mt-2'
+                                value='Submit'>Register
+                            </button>
+                        </div>
+                    </form>
+
+
+                    <text className="text-muted " >
+                        Already have an account?
                     </text>
-                    <input type='text'
-                        placeholder='Full Name'
-                        name='name'
-                        value={user.name}
-                        required
-                        onChange={onChangeInput}
-                        className='form-control form-group' />
-
-                    <text>
-                        Email
-                    </text>
-                    <input type='text'
-                        placeholder='Email'
-                        name='email'
-                        value={user.email}
-                        required
-                        onChange={onChangeInput}
-                        className='form-control form-group' />
-
-                    <text>
-                        Password
-                    </text>
-                    <input type='password'
-                        placeholder='Password'
-                        name='password'
-                        value={user.password}
-                        required
-                        onChange={onChangeInput}
-                        className='form-control form-group' />
-
-                    <text>
-                        Gender
-                    </text>
-
-                    <div className="form-check form-check-inline">
-                        <input type="radio"
-                            id="gender1"
-                            name='gender'
-                            value='male'
-                            onChange={onChangeInput}
-                            className='form-check-input' />
-                        <label className="form-check-label" htmlFor="gender1">Male</label>
-                    </div>
-
-                    <div className="form-check form-check-inline">
-                        <input type="radio"
-                            id="gender2"
-                            name='gender'
-                            value='female'
-                            onChange={onChangeInput}
-                            className='form-check-input' />
-                        <label className="form-check-label" htmlFor="gender2">Female</label>
-                    </div>
-
-                    <text>
-                        Contact
-                    </text>
-                    <input type='number'
-                        placeholder='Contact'
-                        name='contact'
-                        value={user.contact}
-                        required
-                        onChange={onChangeInput}
-                        className='form-control form-group' />
-
-                    <text>
-                        City
-                    </text>
-                    <input type='text'
-                        placeholder='City'
-                        name='city'
-                        value={user.city}
-                        required
-                        onChange={onChangeInput}
-                        className='form-control form-group' />
-
-                    <text>
-                        Province
-                    </text>
-                    <input type='text'
-                        placeholder='Province'
-                        name='province'
-                        value={user.province}
-                        required
-                        onChange={onChangeInput}
-                        className='form-control form-group' />
-
-                    <button type='submit'
-                        className='btn btn-danger btn-block'
-                        value='Submit'>Register
-                    </button>
-                </form>
-
-                <text className="text-muted " >
-                    Already have an account?
-                </text>
-                <a href="/login" class="btn btn-outline-secondary btn-sm" role="button" aria-pressed="true">Login</a>
-
+                    <a href="/login" class="btn btn-outline-secondary btn-sm" role="button" aria-pressed="true">Login</a>
+                </div>
             </div>
         </div>
     )
