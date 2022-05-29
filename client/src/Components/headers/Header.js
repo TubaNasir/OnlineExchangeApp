21ddc73b9882b36299d707378a6adba6e48a74da
Sukaina Batool <sukainasbb@gmail.com>
2022-05-29 15:15:02 +0500

    commit sukaina

--- client/src/Components/headers/Header.js
+++ client/src/Components/headers/Header.js
@@ -2,15 +2,15 @@ import React, { useContext, useState } from 'react'
 import { GlobalState } from '../../GlobalState'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Cart from './cart.svg'
-import {logoutAPI} from '../../api/UserAPI'
-import {IoMdSearch} from 'react-icons/io'
+import { logoutAPI } from '../../api/UserAPI'
+import { IoMdSearch } from 'react-icons/io'
 import './Header.css'
 
 function Header() {
     const state = useContext(GlobalState)
     const [dropdown, setDropdown] = useState(false)
 
-    const handleDropdown = () => {setDropdown(!dropdown)}
+    const handleDropdown = () => { setDropdown(!dropdown) }
     const [isLogged, setIsLogged] = state.UserAPI.isLogged
     const [isAdmin, setIsAdmin] = state.UserAPI.isAdmin
     const [user] = state.UserAPI.user
@@ -20,38 +20,39 @@ function Header() {
 
 
     const router = () => {
-        if (!isAdmin){
-        return (
-            <>
-                <li>{/* <form className="form-inline my-2 my-lg-0 ml-auto ">
+        if (!isAdmin) {
+            return (
+                <>
+                    <li>{/* <form className="form-inline my-2 my-lg-0 ml-auto ">
                     <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
                     <span className="input-group-btn">
                     <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                     </span>
                 </form> */}
-                <div className='searchBar-wrap'>
-                    <IoMdSearch className='searchIcon'/>
-                    <input 
-                    type='text'
-                    placeholder='search'
-                    value={search}
-                    onChange={(e) => setSearch(e.target.value)}/>
-                       
-                      
-                </div>
-                </li>
+                        <div className='searchBar-wrap'>
+                            <IoMdSearch className='searchIcon' />
+                            <input
+                                type='text'
+                                placeholder='search'
+                                value={search}
+                                onChange={(e) => setSearch(e.target.value)} />
 
-                <div className="cart-icon">
+
+                        </div>
+                    </li>
+
+
+                    <div className="cart-icon">
                         <span>0</span>
                         <a href="/cart">
-                        <img src={Cart} alt="" width="25" className="fast"/>
-                        </a>               
+                            <img src={Cart} alt="" width="25" className="fast" />
+                        </a>
                     </div>
-            
-            </>
-        )
+
+                </>
+            )
         }
-        else if (isAdmin){
+        else if (isAdmin) {
             return (
                 <>
                     <li className="nav-item active">
@@ -67,67 +68,69 @@ function Header() {
                         <a className="nav-link" href="/complaints">COMPLAINTS</a>
                     </li>
                 </>
-        )
+            )
         }
     }
 
 
-    const dropdownRouter = ({user}) => {
-        if (!isLogged){
-            return(
-                
+    const dropdownRouter = ({ user }) => {
+        if (!isLogged) {
+            return (
+
                 <a className="nav-link align-right" href="/login">LOGIN/SIGNUP</a>
             )
         }
 
-        else{
-            if(isAdmin){
+        else {
+            if (isAdmin) {
                 const menuClass = `dropdown-menu${dropdown ? "show" : ""}`
                 return (
-                <>
-                <li className="nav-item dropdown ">
-                        <div className="nav-link dropdown-toggle" onClick={handleDropdown} href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            ADMIN
+                    <>
+                        <li className="nav-item dropdown ">
+                            <div className="nav-link dropdown-toggle" onClick={handleDropdown} href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                ADMIN
                             </div>
-                        <div className={menuClass} aria-labelledby="navbarDropdown">
-                            <a className="dropdown-item" href="/">Review Ads</a>
-                            <a className="dropdown-item" href="/">Customer Support</a>
-                            <a className="dropdown-item" href="/category">Categories</a>
-                            <a className="dropdown-item" href="/" onClick={logoutUser}>Logout</a>
-                        
-                        </div>
+                            <div className={menuClass} aria-labelledby="navbarDropdown">
+                                <a className="dropdown-item" href="/">Review Ads</a>
+                                <a className="dropdown-item" href="/">Customer Support</a>
+                                <a className="dropdown-item" href="/category">Categories</a>
+                                <a className="dropdown-item" href="/" onClick={logoutUser}>Logout</a>
+
+                            </div>
                         </li>
-                        </>
-                    
+                    </>
+
                 )
             }
-            else if (!isAdmin){
+            else if (!isAdmin) {
                 const menuClass = `dropdown-menu${dropdown ? "show" : ""}`
                 return (
-                <>
-                <li className="nav-item dropdown ">
-                        <div className="nav-link dropdown-toggle" onClick={handleDropdown} href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            {user.name}
+                    <>
+                        <li className="nav-item dropdown ">
+                            <div className="nav-link dropdown-toggle" onClick={handleDropdown} href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                {user.name}
                             </div>
-                        <div className={menuClass} aria-labelledby="navbarDropdown">
-                            <a className="dropdown-item" href="/">My Profile</a>
-                            <div className="dropdown-divider"></div>
-                            <a className="dropdown-item" href="/">My Ads</a>
-                            <div className="dropdown-divider"></div>
-                            <a className="dropdown-item" href="/">My Orders</a>
-                            <div className="dropdown-divider"></div>
-                            <a className="dropdown-item" href="/">My Favourites</a>
-                            <div className="dropdown-divider"></div>
-                            <a className="dropdown-item" href="/">Customer Support</a>
-                            <div className="dropdown-divider"></div>
-                            <a className="dropdown-item" href="/" onClick={logoutUser}>Logout</a>
-                        </div>
-                    </li>
+                            <div className={menuClass} aria-labelledby="navbarDropdown">
+                                <a className="dropdown-item" href="/">My Profile</a>
+                                <div className="dropdown-divider"></div>
+                                <a className="dropdown-item" href="/">My Ads</a>
+                                <div className="dropdown-divider"></div>
+                                <a className="dropdown-item" href="/">My Orders</a>
+                                <div className="dropdown-divider"></div>
+                                <a className="dropdown-item" href="/">My Favourites</a>
+                                <div className="dropdown-divider"></div>
+                                <a className="dropdown-item" href="/">Customer Support</a>
+                                <div className="dropdown-divider"></div>
+                                <a className="dropdown-item" href="/msg">Messages</a>
+                                <div className="dropdown-divider"></div>
+                                <a className="dropdown-item" href="/" onClick={logoutUser}>Logout</a>
+                            </div>
+                        </li>
                     </>
                 )
             }
-        }  
-        
+        }
+
     }
 
     const logoutUser = async () => {
@@ -145,24 +148,24 @@ function Header() {
     }
 
     return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a className="navbar-brand" href="/">X-CHANGE</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="/">X-CHANGE</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
-                </button>
+            </button>
 
-                <div className="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
-                    <ul className="navbar-nav justify-content-between">
-                        {router()}
-                    </ul>
-                    <ul className="navbar-nav mr-auto">
-                        {dropdownRouter({user})}
-                    </ul>
-                </div>
+            <div className="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+                <ul className="navbar-nav justify-content-between">
+                    {router()}
+                </ul>
+                <ul className="navbar-nav mr-auto">
+                    {dropdownRouter({ user })}
+                </ul>
+            </div>
 
-            </nav>
-               
-                
+        </nav>
+
+
     )
 }
 
--- client/src/Components/mainpages/PageRoutes.js
+++ client/src/Components/mainpages/PageRoutes.js
@@ -1,6 +1,6 @@
-import React,  {useContext} from 'react';
+import React, { useContext } from 'react';
 import { GlobalState } from '../../GlobalState'
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Register from './Register';
 import Login from './Login';
 import Advertisements from './Advertisements'
@@ -11,28 +11,30 @@ import AdminHomepage from './AdminHomepage'
 import Profile from './Profile'
 import Category from './Category'
 import AdvertisementsFiltered from './AdvertisementsFiltered';
+import Msg from './Msg'
 
 
 
 function PageRoutes() {
 
   const state = useContext(GlobalState)
-  
+
   const [isLogged, setIsLogged] = state.UserAPI.isLogged
   const [isAdmin, setIsAdmin] = state.UserAPI.isAdmin
 
   return (
     <Routes>
-        
-        <Route path="/login" exact element={<Login/>}/>
-        <Route path="/register" exact element={<Register/>}/>
-        <Route path="/" exact element={isAdmin ? <AdminHomepage/> : <Advertisements/>}/>
-        <Route path="/:slug" exact element={isAdmin ? <AdminHomepage/> : <Advertisements/>}/>
-        <Route path="/users" exact element={<Users/>}/>
-        <Route path="/orders" exact element={<Orders/>}/>
-        <Route path="/complaints" exact element={<Complaints/>}/>
-        <Route path="/profile" exact element={<Profile/>}/>
-        <Route path="/category" exact element={<Category/>}/>
+
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/register" exact element={<Register />} />
+      <Route path="/" exact element={isAdmin ? <AdminHomepage /> : <Advertisements />} />
+      <Route path="/:slug" exact element={isAdmin ? <AdminHomepage /> : <Advertisements />} />
+      <Route path="/users" exact element={<Users />} />
+      <Route path="/orders" exact element={<Orders />} />
+      <Route path="/complaints" exact element={<Complaints />} />
+      <Route path="/profile" exact element={<Profile />} />
+      <Route path="/category" exact element={<Category />} />
+      <Route path="/msg" exact element={<Msg />} />
     </Routes>
   )
 }
